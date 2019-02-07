a4cf487c268279fa9ddd76c8bfda02b76878dce8
Tavia Sterling <TaviaSterling@outlook.com>
2019-02-07 16:13:40 -0700

    fix program

--- JewelThief/Program.cs
+++ JewelThief/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace JewelThief
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Owner owner = new Owner();
+            Safe safe = new Safe();
+            JewelThief jewelThief = new JewelThief();
+            jewelThief.OpenSafe(safe, owner);
+            Console.ReadKey();
         }
     }
 }
